d5c012a8fc983c9788bb03d8a1506b2240bea17e
macmade <macmade@xs-labs.com>
2020-09-11 02:55:39 +0200

    Makefile

---
+++ Make/Config.mk
@@ -0,0 +1,152 @@
+#-------------------------------------------------------------------------------
+# The MIT License (MIT)
+# 
+# Copyright (c) 2020 Jean-David Gadina - www.xs-labs.com
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+# Paths
+#-------------------------------------------------------------------------------
+
+# Root directory
+DIR := ./
+
+# Root build directory
+DIR_BUILD := $(DIR)Build/
+
+# Relative build directories
+DIR_BUILD_PRODUCTS := $(DIR_BUILD)Products/
+DIR_BUILD_TEMP     := $(DIR_BUILD)Intermediates/
+
+# Source directories
+DIR_INC := $(DIR)XSCTest/include/
+DIR_SRC := $(DIR)XSCTest/source/
+
+#-------------------------------------------------------------------------------
+# Search paths
+#-------------------------------------------------------------------------------
+
+# Clear any existing search path
+VPATH =
+vpath
+
+# Define the search paths for source files
+vpath %$(EXT_C) $(DIR_SRC)
+
+#-------------------------------------------------------------------------------
+# File suffixes
+#-------------------------------------------------------------------------------
+
+# File extensions
+EXT_C         := .c
+EXT_H         := .h
+EXT_O         := .o
+EXT_LIB       := .a
+
+# Adds the suffixes used in this file
+.SUFFIXES: $(EXT_C) $(EXT_H) $(EXT_O)
+
+#-------------------------------------------------------------------------------
+# Files
+#-------------------------------------------------------------------------------
+
+# 
+# Gets every C file in a specific source directory
+# 
+# @param    The directory
+# 
+GET_C_FILES = $(foreach dir,$(1), $(wildcard $(dir)*$(EXT_C)))
+
+# Gets only the file name of the C files
+_FILES_C_REL = $(subst $(DIR_SRC),,$(FILES_C))
+
+# Replace the code extension by the object one
+_FILES_C_OBJ = $(subst $(EXT_C),$(EXT_O),$(_FILES_C_REL))
+
+# Prefix all object files with the build directory
+_FILES_C_BUILD = $(addprefix $(DIR_BUILD_TEMP),$(_FILES_C_OBJ))
+
+#-------------------------------------------------------------------------------
+# Commands configuration
+#-------------------------------------------------------------------------------
+
+# Build flags
+_CC_FLAGS = $(OPTIM) -DDEBUG=1 -gfull
+
+# C compiler
+_CC = $(CC) -Werror -Weverything -std=$(STDC) -I$(DIR_INC) $(_CC_FLAGS)
+
+#-------------------------------------------------------------------------------
+# Display
+#-------------------------------------------------------------------------------
+
+# Terminal colors
+COLOR_NONE                              := "\x1b[0m"
+COLOR_GRAY                              := "\x1b[30;01m"
+COLOR_RED                               := "\x1b[31;01m"
+COLOR_GREEN                             := "\x1b[32;01m"
+COLOR_YELLOW                            := "\x1b[33;01m"
+COLOR_BLUE                              := "\x1b[34;01m"
+COLOR_PURPLE                            := "\x1b[35;01m"
+COLOR_CYAN                              := "\x1b[36;01m"
+
+# 
+# Prints a message to the standard output
+# 
+# @param    The prompt components
+# @param    The message
+# 
+PRINT = @echo "[ "$(COLOR_CYAN)XSCTest$(COLOR_NONE) "]> [ "$(COLOR_PURPLE)$(MAKELEVEL)$(COLOR_NONE) "]> "$(foreach _P,$(_BRANCH) $(1),"[ "$(COLOR_GREEN)$(_P)$(COLOR_NONE)" ]>")" *** "$(2)
+
+# 
+# Prints an architecture related message to the standard output
+# 
+# @param    The architecture
+# @param    The message
+# 
+PRINT_ARCH = $(call PRINT,,$(2) [ $(COLOR_RED)$(1)$(COLOR_NONE) ])
+
+# 
+# Prints an architecture related message about a file to the standard output
+# 
+# @param    The architecture
+# @param    The message
+# @param    The file
+# 
+PRINT_FILE = $(call PRINT_ARCH,$(1),$(2)): $(COLOR_YELLOW)$(patsubst %.,%,$(subst /,.,$(dir $(patsubst $(DIR_SRC)%,%,$3))))$(COLOR_NONE).$(COLOR_GRAY)"$(notdir $(3))"$(COLOR_NONE)
+
+#-------------------------------------------------------------------------------
+# Tools
+#-------------------------------------------------------------------------------
+
+# Make version (version 4 allows parallel builds with output sync) 
+_MAKE_VERSION_MAJOR := $(shell echo $(MAKE_VERSION) | cut -f1 -d.)
+_MAKE_4             := $(shell [ $(_MAKE_VERSION_MAJOR) -ge 4 ] && echo true)
+
+#-------------------------------------------------------------------------------
+# Miscellaneous
+#-------------------------------------------------------------------------------
+
+# Host architecture
+_HOST_ARCH := $(shell uname -m)
+
+# Current GIT branch
+_BRANCH := $(shell git rev-parse --abbrev-ref HEAD 2>/dev/null)
---
+++ Make/Targets.mk
@@ -0,0 +1,88 @@
+#-------------------------------------------------------------------------------
+# The MIT License (MIT)
+# 
+# Copyright (c) 2020 Jean-David Gadina - www.xs-labs.com
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+# Built-in targets
+#-------------------------------------------------------------------------------
+
+# Default make target
+.DEFAULT_GOAL := all
+
+# Declaration for phony targets, to avoid problems with local files
+.PHONY: all clean build lib
+
+# Declaration for precious targets, to avoid cleaning of intermediate files
+.PRECIOUS: $(DIR_BUILD_TEMP)%$(EXT_O)
+
+#-------------------------------------------------------------------------------
+# Phony targets
+#-------------------------------------------------------------------------------
+
+# Main Target
+all: lib
+	
+	@:
+
+# Cleans all build files
+clean:
+	
+	$(call PRINT_ARCH,$(_HOST_ARCH),Cleaning all intermediate files)
+	@rm -rf $(DIR_BUILD_TEMP)*
+	
+	$(call PRINT_ARCH,$(_HOST_ARCH),Cleaning all product files)
+	@rm -rf $(DIR_BUILD_PRODUCTS)*
+	
+
+# Build (parallel if available)
+build:
+	
+ifeq ($(_MAKE_4),true)
+	@$(MAKE) -s -j 50 --output-sync lib
+else
+	@$(MAKE) lib
+endif
+
+#-------------------------------------------------------------------------------
+# Targets with second expansion
+#-------------------------------------------------------------------------------
+
+.SECONDEXPANSION:
+
+# Static library
+lib: _OBJ = $(DIR_BUILD_TEMP)XSCTest$(EXT_O)
+lib: _LIB = $(DIR_BUILD_PRODUCTS)$(PRODUCT)$(EXT_LIB)
+lib: $$(_FILES_C_BUILD)
+
+	$(call PRINT_ARCH,$(_HOST_ARCH),"Linking object files"): $(COLOR_BLUE)$(notdir $(_OBJ))$(COLOR_NONE)
+	@rm -f $(_OBJ)
+	@ld -r $(_FILES_C_BUILD) -o $(_OBJ)
+	
+	$(call PRINT_ARCH,$(_HOST_ARCH),"Creating static library"): $(COLOR_BLUE)$(notdir $(_LIB))$(COLOR_NONE)
+	@libtool -static -o $(_LIB) $(_OBJ)
+
+# Target: Object file
+$(DIR_BUILD_TEMP)%$(EXT_O): $$(shell mkdir -p $$(dir $$@)) %$(EXT_C)
+	
+	$(call PRINT_FILE,$(_HOST_ARCH),"Compiling C file",$<)
+	@$(_CC) -o $@ -c $<
