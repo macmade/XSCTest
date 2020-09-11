6e82962add87fb61351898c72fd7db60810acb9e
macmade <macmade@xs-labs.com>
2020-09-11 03:16:33 +0200

    Preparing for floating point comparison.

--- Makefile
+++ Makefile
@@ -39,4 +39,4 @@ STDC    := c11
 #-------------------------------------------------------------------------------
 
 # Gets every C file in the source directories
-FILES_C += $(call GET_C_FILES, $(DIR_SRC))
+FILES_C += $(call GET_C_FILES, $(DIR_SRC)FloatingPoint/)
---
+++ XSCTest/include/XSCTest/FloatingPoint.h
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2020 Jean-David Gadina - www.xs-labs.com
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ******************************************************************************/
+
+/*!
+ * @header      FloatingPoint.h
+ * @copyright   (c) 2020 - Jean-David Gadina - www.xs-labs.com
+ * @author      Jean-David Gadina - www.xs-labs.com
+ */
+
+#ifndef XSCTEST_FLOATING_POINT_H
+#define XSCTEST_FLOATING_POINT_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+bool     XSCTestFloatEqual( float v1, float v2 );
+bool     XSCTestDoubleEqual( double v1, double v2 );
+uint64_t XSCTestFloatDistance( float v1, float v2 );
+uint64_t XSCTestDoubleDistance( double v1, double v2 );
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* XSCTEST_FLOATING_POINT_H */
