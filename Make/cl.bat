23c5c14ef17dad08e32cc35ee79caef6f7d0788a
macmade <macmade@xs-labs.com>
2020-09-22 23:20:31 +0200

    Now using VS 2019 Preview for C11.

--- Make/cl.bat
+++ Make/cl.bat
@@ -1,1 +1,1 @@
-%comspec% /c ""c:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64 && cl.exe %*"
+%comspec% /c ""c:\Program Files (x86)\Microsoft Visual Studio\2019\Preview\VC\Auxiliary\Build\vcvarsall.bat" amd64 && cl.exe %*"
