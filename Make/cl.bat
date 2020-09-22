8ab67f8c0ce6e82a14c8677dd755a70ae24efcbf
macmade <macmade@xs-labs.com>
2020-09-22 23:43:08 +0200

    vcvars - don't print...

--- Make/cl.bat
+++ Make/cl.bat
@@ -1,1 +1,2 @@
-%comspec% /c ""c:\Program Files (x86)\Microsoft Visual Studio\2019\Preview\VC\Auxiliary\Build\vcvarsall.bat" amd64 && cl.exe %*"
+@echo off
+%comspec% /c ""c:\Program Files (x86)\Microsoft Visual Studio\2019\Preview\VC\Auxiliary\Build\vcvarsall.bat" >NUL amd64 && cl.exe %*"
